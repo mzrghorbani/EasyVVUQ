b875134da192f9227a6acdb964543b7e2eef6806
Derek Groen <djgroennl@gmail.com>
2024-06-25 13:37:09 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../easyvvuq'))
+sys.path.insert(0, os.path.abspath('easyvvuq'))
 
 from sphinx.ext.apidoc import main as apidoc_main
 apidoc_main(["--force", "-o", "./_autodoc", "../../easyvvuq"])
